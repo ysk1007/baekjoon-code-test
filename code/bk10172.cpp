9160fb169fbd15915848d73906f20c0debf48e8e
okonys <yoonsg1007@gmail.com>
2025-04-02 17:54:58 +0900

    2025-04-02 새싹 반복 문제 1개
    런타임 에러 해결

---
+++ code/bk10172.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+// 개 #10172
+int main(){
+    string dog = "|\\_/|\n|q p|   /}";
+}
---
+++ code/bk2741.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+// N찍기 #2741
+int main(){
+    int num;
+    cin >> num;
+    for(int i = 1 ; i <= num ; i++){
+        cout << i << '\n';
+    }
+}
