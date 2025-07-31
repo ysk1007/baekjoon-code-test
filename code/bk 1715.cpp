c1d71d2580f335b74ddb87e08d9b07361744f78d
okonys <62321546+ysk1007@users.noreply.github.com>
2025-07-31 17:44:04 +0900

    2025.07.31 골드 1개, 실버 1개
    우선순위 큐 라이브러리로 해결했음.
    우선순위 큐 직접 구현해보자

--- code/bk 1715.cpp
+++ code/bk 1715.cpp
@@ -1,59 +1,40 @@
 #include<iostream>
 #include<vector>
-#include<deque>
-#include<algorithm>
+#include<queue>
 using namespace std;
 
 // 카드 정렬하기 #1715
 
 // greedy : 매 순간 최선의 선택 해야함
+// 매 순간 가장 적은 뭉치(가장 높은 우선순위)끼리 비교 해야함
+// 우선순위 큐 사용
 
-// 매 순간 가장 적은 뭉치끼리 비교 해야함
-// 비교 완료한 뭉치가 데크에 정렬된 위치로 들어가기는 어려움 => 일단 temp에 저장
-// 다시 비교
-// 데크에서 두 장 뽑고 temp(뭉쳐졌지만 그래도 가장 작을 수 있음)랑 셋이서 비교
-// 
-// S1. temp, i, j 세장 비교 => 작은 뭉치끼리 섞음 => k 와 나머지 비교 => 큰 쪽이 temp가되고 
-// S3. 데크에 하나만 남아서 두 장 비교 (이제 끝)
 int main(){
     int t, n;
     long long result = 0;
     cin >> t;
-    vector<int> list(t);
-    deque<long long> deq;
+
+    priority_queue<long long, vector<long long>, greater<long long>> pq;
 
     for(int i = 0 ; i < t ; i++){
-        cin >> list[i];
+        cin >> n;
+        pq.push(n);
     }
 
-    sort(list.begin(),list.end());
+    while(true){
+        long long i = pq.top();
+        pq.pop();
 
-    for(auto l : list){
-        deq.push_back(l);
-    }
-
-    long long temp = deq.front();
-    deq.pop_front();
-
-    while(!deq.empty()){
-        long long i = deq.front();
-        deq.pop_front();
-
-        if(!deq.empty()){
-            long long j = deq.front();
-            deq.pop_front();
-            
-            
-
-            result += j + i;
-            temp = j + i;
+        if(pq.empty()){
+            cout << result;
+            return 0;
         }
-        // S3.
-        else{
-            result += temp + i;
-            temp = temp + i;
-        }
-    }
 
-    cout << result;
+        long long j = pq.top();
+        pq.pop();
+
+        result += i + j;
+        long long k = i + j;
+        pq.push(k);
+    }
 }
