b104a2b113de85811e84813572b9ac71badc1c17
okonys <62321546+ysk1007@users.noreply.github.com>
2025-08-05 17:51:02 +0900

    2025.08.05 실버 1개
    실버 문제 1개 해결

--- code/bk2156.cpp
+++ code/bk2156.cpp
@@ -20,17 +20,22 @@ int main(){
 
     dp[1] = al[0];
     dp[2] = al[0] + al[1];
-    
+
+    if(t == 1) {cout << dp[1]; return 0;}
+    if(t == 2) {cout << dp[2]; return 0;}
+
     for(int i = 3 ; i <= t ; i++){
         int case1 = dp[i-2] + al[i-1];
-        int case2 = dp[i-3] + al[i-1] + al[i-3];
-        dp[i] = al[i-1] + max(case1,case2);
+        int case2 = dp[i-3] + al[i-1] + al[i-2];
+        int case3 = dp[i-1];
+
+        dp[i] = max(case3,max(case1,case2));
     }
 
     // 디버깅
-    for(int i = 0 ; i <= t ; i++){
-        cout << i << ": " << dp[i] << "\n";
-    }
+    // for(int i = 0 ; i <= t ; i++){
+    //     cout << i << ": " << dp[i] << "\n";
+    // }
 
-    cout << "결과 : " << dp[t];
+    cout << dp[t];
 }
